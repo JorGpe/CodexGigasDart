3f302989aecc20b65ca5b1426a286f8903605c73
JorGpe <jorge.padilla5743@alumnos.udg.mx>
2025-05-08 09:17:33 -0600

    cambio nombre carpetas
    categorizacion mejor explicada de las carpetas

--- api/main.py
+++ api/main.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 
-from functions import punto_fijo
+from api.functions.ecu_no_poli import punto_fijo
 
 app = Flask(__name__)
 CORS(app) # This will enable CORS for all routes
